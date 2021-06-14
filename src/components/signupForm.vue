a03e4cc4c259d8532ac9e12fb27974f23608ea84
Niusha <ext.n.yousefi@mci.ir>
2021-06-14 20:34:19 +0430

    delete span tags

--- src/components/signupForm.vue
+++ src/components/signupForm.vue
@@ -11,12 +11,23 @@
           <option value="developer">web developer</option>
           <option value="designer">web designer</option>
       </select>
- 
+
+      <label>Skills: </label>
+      <input type="text" v-model="tempSkill" @keyup="addSkill">
+      <div v-for="skill in skills" :key="skill" class="pill">
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
+      </div>
+
+      <div class="terms">
+          <input type="checkbox" required v-model="terms">
+          <label>Accept terms and conditions</label>
+      </div>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms Accepted: {{ terms }}</p>
 </template>
 
 <script>
@@ -25,7 +36,25 @@ export default {
         return {
             email: '',
             password: '',
-            role: 'designer'
+            role: 'designer',
+            terms: false,
+            tempSkill: '',
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if(e.key === ',' && this.tempSkill) {
+                if (!this.skills.includes(this.tempSkill)) {
+                    this.skills.push(this.tempSkill.slice(0, -1))
+              }
+                    this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill !== item 
+            })
         }
     },
 }
@@ -59,5 +88,26 @@ export default {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+  }
+
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
   }
 </style>
